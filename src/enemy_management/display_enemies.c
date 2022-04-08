52ac14dabb88a7ec2f2a2cd6045386ac022b31c8
Antonin Laudon <alaudon@pop-os.localdomain>
2022-04-08 21:59:26 +0200

    added: enemy initialization and displaying

---
+++ src/enemy_management/display_enemies.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2022
+** B-MUL-200-TLS-2-1-myrpg-luca.deltort
+** File description:
+** display_enemies
+*/
+
+#include <SFML/Graphics.h>
+#include "enemies.h"
+#include "level.h"
+#include "structure.h"
+
+void display_enemy(sfRenderWindow *window, enemy_t *enemy)
+{
+    if (enemy->alive == sfTrue && enemy->type != 0) {
+        sfSprite_setPosition(enemy->sprite, enemy->pos);
+        sfRenderWindow_drawSprite(window, enemy->sprite, NULL);
+    }
+}
+
+void display_enemies(game_t *game, enum levels level)
+{
+    enemy_t **enemy = game->levels[level]->enemies;
+    sfRenderWindow *window = game->window;
+
+    for (int i = 0; enemy[i]; i++)
+        display_enemy(window, enemy[i]);
+}
---
+++ src/enemy_management/enemy_init.c
@@ -0,0 +1,74 @@
+/*
+** EPITECH PROJECT, 2022
+** B-MUL-200-TLS-2-1-myrpg-luca.deltort
+** File description:
+** enemy_init
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "enemies.h"
+#include "level.h"
+#include "structure.h"
+#include "prototype.h"
+#include "sprite_sheet.h"
+
+enum enemy_type get_enemy_type(char *str)
+{
+    if (my_strcmp(str, "Purple") == 0)
+        return Purple;
+    if (my_strcmp(str, "Hood") == 0)
+        return Hood;
+    return Error;
+}
+
+enemy_t *enemy_create(sfVector2f idle_around, char *name, game_t *game)
+{
+    enemy_t *enemy = malloc(sizeof(enemy_t));
+    enemy_jump_t *jump = malloc(sizeof(enemy_jump_t));
+
+    if (!enemy || !jump)
+        return NULL;
+    enemy->type = get_enemy_type(name);
+    enemy->sprite = NULL;
+    if (enemy->type != Error)
+        enemy->sprite = sfSprite_copy(game->all_sprite[Player][PlayerGun1]);
+    jump->is_jumping = sfFalse;
+    enemy->player_pos = (sfVector2f){-1, -1};
+    enemy->pos = idle_around;
+    enemy->idle_around = idle_around;
+    enemy->jump_rate = 1000;
+    enemy->alive = sfTrue;
+    enemy->idled_since = 0;
+    enemy->jump = jump;
+    enemy->behaviour = Idle;
+    return enemy;
+}
+
+enemy_t **enemies_create(char *enemies_data, game_t *game)
+{
+    char **enemies = my_str_to_word_array(enemies_data, "\n");
+    int nbr_enemies = my_atoi(enemies[0]);
+    enemy_t **enemy_array = malloc(sizeof(enemy_t *) * (nbr_enemies + 1));
+    char **enemy = NULL;
+
+    enemy_array[nbr_enemies] = NULL;
+    for (int i = 0; i < nbr_enemies; i++) {
+        enemy = my_str_to_word_array(enemies[i + 1], " ");
+        enemy_array[i] = enemy_create((sfVector2f){my_atoi(enemy[1]), my_atoi(enemy[2])}, enemy[0], game);
+        my_free_word_array(enemy);
+    }
+    my_free_word_array(enemies);
+    return enemy_array;
+}
+
+void enemy_destroy(enemy_t *enemy)
+{
+    if (!enemy)
+        return;
+    if (enemy->type != Error)
+        sfSprite_destroy(enemy->sprite);
+    free(enemy->jump);
+    free(enemy);
+    return;
+}
