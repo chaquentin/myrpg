03f8b80858981211858e21133789b65526682596
Antonin Laudon <alaudon@pop-os.localdomain>
2022-04-09 23:57:18 +0200

    added: initialization of the sfView

---
+++ src/view_management/view_init.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2022
+** B-MUL-200-TLS-2-1-myrpg-luca.deltort
+** File description:
+** view_init
+*/
+
+#include <SFML/Graphics.h>
+#include "structure.h"
+
+sfView *create_view(game_t *game, int debug)
+{
+    sfView *view = NULL;
+    sfVector2f pos = {0, 0};
+
+    if (debug)
+        return NULL;
+    view = sfView_create();
+    sfView_setCenter(view, pos);
+    sfView_setSize(view, (sfVector2f){960, 540});
+    sfView_zoom(view, 1);
+    sfRenderWindow_setView(game->window, view);
+    return view;
+}
