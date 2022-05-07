f9bf05c081cc75ac8ab9ca7696adcf309bef2a74
Matthias SOUAL <matthias.soual@epitech.eu>
2022-05-08 00:42:36 +0200

    ADD shop; change the mf gun

--- src/npc_management/matthias_action.c
+++ src/npc_management/matthias_action.c
@@ -19,32 +19,57 @@ int *window_open)
     return (0);
 }
 
+int show_weapon(game_t *game, player_t *player)
+{
+    sfVector2f pos = sfView_getCenter(game->view);
+    while (1) {
+        sfRenderWindow_clear(game->window, sfBlack);
+        sfSprite_setPosition(game->all_sprite[Weapon][20], pos);
+        sfRenderWindow_drawSprite(game->window, game->all_sprite[Weapon][20], NULL);
+    }
+    return (0);
+}
+
 int matthias_action(game_t *game, player_t *player)
 {
     int window_open = 1;
     int current_weapon = 12;
     sfEvent event;
     sfVector2f pos = {0, 0};
+    float clock = 0;
     pos = sfView_getCenter(game->view);
+            sfClock *cock;
     pos.x -= 3*32;
     pos.y -= 3*32;
 
-    //for (int i = 12; i != 23; i++)
-    //    sfSprite_scale(game->all_sprite[Weapon][i], (sfVector2f){3, 3});
+    int speed = 2000;
+
+    int k = get_randint(12, 22);
+
+    for (int i = 12; i != 23; i++)
+        sfSprite_scale(game->all_sprite[Weapon][i], (sfVector2f){3, 3});
 
     write_dialogue(game, game->all_npc[Matthias - Antonin]->all_dialogs[0]);
+
+
     while (window_open) {
         update_clock(game);
-        pos.x -= 100 * game->delta_time;
+        clock += game->delta_time;
+        sfRenderWindow_clear(game->window, sfWhite);
         get_matthias_event(game, player, &event, &window_open);
-        if (current_weapon > 22)
-            current_weapon = 12;
-        sfRenderWindow_clear(game->window, sfBlack);
-        sfSprite_setPosition(game->all_sprite[Weapon][current_weapon], pos);
-        sfRenderWindow_drawSprite(game->window, game->all_sprite[Weapon][20], NULL);
-        current_weapon++;
-        printf("%f\n", 100 * game->delta_time);
+        if (clock < 2) {
+            int j = get_randint(12, 22);
+            sfSprite_setPosition(game->all_sprite[Weapon][j], pos);
+            sfRenderWindow_drawSprite(game->window, game->all_sprite[Weapon][j], NULL);
+        }
+        else {
+            sfSprite_setPosition(game->all_sprite[Weapon][k], pos);
+            sfRenderWindow_drawSprite(game->window, game->all_sprite[Weapon][k], NULL);
+        }
         sfRenderWindow_display(game->window);
     }
+    for (int i = 12; i != 23; i++)
+        sfSprite_scale(game->all_sprite[Weapon][i], (sfVector2f){0.333, 0.333});
+    change_weapon(player, game, k - 12);
     return (0);
 }
